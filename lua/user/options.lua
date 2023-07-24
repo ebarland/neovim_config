4d512b74eade13aab0fa51fdfd34975017a97788
Egil Barland <egil87@gmail.com>
2023-07-24 15:03:04 +0200

    treesitter script

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -39,3 +39,25 @@ o.scrolloff = 4                 -- keep 4 lines off the edges of the screen when
 	vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 end
 
+local treesitter_status, _ = pcall(require, "nvim-treesitter")
+if not treesitter_status then
+	print("treesitter not found")
+else
+	require'nvim-treesitter.configs'.setup {
+		-- A list of parser names, or "all" (the five listed parsers should always be installed)
+		ensure_installed = { "c", "lua", "cpp", "vim", "vimdoc", "query" },
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = false,
+		-- Automatically install missing parsers when entering buffer
+		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+		auto_install = true,
+		-- List of parsers to ignore installing (for "all")
+		ignore_install = { "javascript" },
+		---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+		-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false
+		}
+	}
+end
