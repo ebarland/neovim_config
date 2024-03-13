68ae8636555c64e8cc7223fd5a0dc028a2009485
Egil Barland <egil87@gmail.com>
2024-03-13 13:07:19 +0100

    copy neovim config to git = leader-c-t-g

--- mappings.lua
+++ mappings.lua
@@ -3,75 +3,92 @@
 local M = {}
 
 M.disabled = {
-  n = {
-    ["<leader>b"] = "",
-    ["<leader>e"] = "",
-    ["<C-n>"] = "",
-    ["<C-.>"] = ""
-  }
+	n = {
+		["<leader>b"] = "",
+		["<leader>e"] = "",
+		["<C-n>"] = "",
+		["<C-.>"] = ""
+	}
 }
 
+--| Variable       | Default Value                                                                |
+--|----------------|------------------------------------------------------------------------------|
+--| %SystemDrive%  | C:                                                                           |
+--| %ProgramFiles% | C:\Program Files                                                             |
+--| %AppData%      | C:\Users\{username}\AppData\Roaming                                          |
+--| %LocalAppData% | C:\Users\{username}\AppData\Local                                            |
+--| %UserProfile%  | C:\Users\{username}                                                          |
+--| %UserName%     | {username}                                                                   |
+--| %COMPUTERNAME% | {computername}                                                               |
+--| %PATH%         | C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;{plus program paths} |
+
+local config_dir = vim.fn.stdpath("config")
+
 M.custom = {
-  plugin = false,
-  n = {
-    ["<leader>cc"] = {
-      ":cd C:\\Users\\Egil\\AppData\\Local\\nvim<CR>:NvimTreeFocus<CR>",
-      "Move to and open NVIM config directory"
-    },
-    ["<leader>cd"] = {
-      ":cd C:\\Development\\Git<CR>:NvimTreeFocus<CR>",
-      "Move to and open GIT directory"
-    },
-    ["<leader>bb"] = {
-      ":! .\\scripts\\build.bat<CR>",
-      "runs build.bat"
-    },
-    ["<leader>br"] = {
-      ":! .\\scripts\\run.bat<CR>",
-      "runs run.bat"
-    },
-    ["<leader>bd"] = {
-      ":! .\\scripts\\debug.bat<CR>",
-      "runs debug.bat"
-    }
-  }
+	plugin = false,
+	n = {
+		["<leader>cc"] = {
+			":cd " ..config_dir.. "<CR>:NvimTreeFocus<CR>",
+			"Move to and open NVIM config directory"
+		},
+		["<leader>cd"] = {
+			":cd %SystemDrive%\\Git<CR>:NvimTreeFocus<CR>",
+			"Move to and open GIT directory"
+		},
+		["<leader>ctg"] = {
+			":! xcopy /s /y "..config_dir.. "\\lua\\custom C:\\Development\\Git\\Private\\neovim_config<CR>",
+			"Copies and overwrites the local neovim config to neovim_git folder"
+		},
+		["<leader>bb"] = {
+			":! .\\scripts\\build.bat<CR>",
+			"runs build.bat"
+		},
+		["<leader>br"] = {
+			":! .\\scripts\\run.bat<CR>",
+			"runs run.bat"
+		},
+		["<leader>bd"] = {
+			":! .\\scripts\\debug.bat<CR>",
+			"runs debug.bat"
+		}
+	}
 }
 
 M.dap = {
-  plugin = true,
-  n = {
-    ["<F5>"] = {
-      "<cmd> DapContinue <CR>",
-      "Start/continue debugger"
-    },
-    ["<S-F5>"] = {
-      "<cmd> DapTerminate <CR>",
-      "Start/continue debugger"
-    },
-    ["<F9>"] = {
-      "<cmd> DapToggleBreakpoint <CR>",
-      "Add/remove breakpoint"
-    },
-    ["<F10>"] = {
-      "<cmd> DapStepOver <CR>",
-      "Step over"
-    },
-    ["<F11>"] = {
-      "<cmd> DapStepInto <CR>",
-      "Step into"
-    },
-    ["<F4>"] = {
-      "<Cmd>lua require\"dapui\".toggle()<CR>",
-      "Toggle DAP-UI"
-    }
-  }
+	plugin = true,
+	n = {
+		["<F5>"] = {
+			"<cmd> DapContinue <CR>",
+			"Start/continue debugger"
+		},
+		["<S-F5>"] = {
+			"<cmd> DapTerminate <CR>",
+			"Start/continue debugger"
+		},
+		["<F9>"] = {
+			"<cmd> DapToggleBreakpoint <CR>",
+			"Add/remove breakpoint"
+		},
+		["<F10>"] = {
+			"<cmd> DapStepOver <CR>",
+			"Step over"
+		},
+		["<F11>"] = {
+			"<cmd> DapStepInto <CR>",
+			"Step into"
+		},
+		["<F4>"] = {
+			"<Cmd>lua require\"dapui\".toggle()<CR>",
+			"Toggle DAP-UI"
+		}
+	}
 }
 
 M.nvimtree = {
-  plugin = true,
-  n = {
-    ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" }
-  }
+	plugin = true,
+	n = {
+		["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" }
+	}
 }
 
 return M
