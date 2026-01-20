38063506c4e1f82e587b8c9c9be9f8f604d68c69
Egil Barland <eb@nextcom-norge.no>
2026-01-20 13:16:13 +0100

    Pin treesitter to master branch, wait with main until stable

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,54 @@
 -- lua/plugins/treesitter.lua
 return {
 	"nvim-treesitter/nvim-treesitter",
-	lazy = false, -- main branch: does not support lazy-loading
+	branch = "master",
 	build = ":TSUpdate",
 	config = function()
-		local ts = require("nvim-treesitter")
-
-		-- Keep parsers/queries in a predictable, cross-platform location
-		ts.setup({
-			install_dir = vim.fn.stdpath("data") .. "/site",
-		})
-
-		-- Install parsers (async). :TSUpdate (above) keeps them in sync.
-		ts.install({
-			-- Front-end
-			"vue", "typescript", "javascript", "html", "css", "scss", "json", "tsx",
-			-- Back-end
-			"c", "cpp", "lua", "python", "rust", "cmake",
-		})
-
-		-- Enable highlighting via Neovim (main branch expects this)
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function(ev)
-				-- highlight
-				pcall(vim.treesitter.start, ev.buf)
-
-				-- indent (experimental), but disable for c/cpp like you had before
-				local ft = vim.bo[ev.buf].filetype
-				if ft ~= "c" and ft ~= "cpp" then
-					vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-				end
-			end,
-		})
+		require("nvim-treesitter.configs").setup {
+			indent = { enable = true, disable = { "c", "cpp" } },
+			ensure_installed = {
+				"vue", "typescript", "javascript", "html", "css", "scss", "json", "tsx",
+				"c", "cpp", "lua", "python", "rust", "cmake",
+			},
+			highlight = { enable = true, additional_vim_regex_highlighting = false },
+		}
 	end,
 }
+
+
+-- -- lua/plugins/treesitter.lua
+-- return {
+-- 	"nvim-treesitter/nvim-treesitter",
+-- 	lazy = false, -- main branch: does not support lazy-loading
+-- 	build = ":TSUpdate",
+-- 	config = function()
+-- 		local ts = require("nvim-treesitter")
+--
+-- 		-- Keep parsers/queries in a predictable, cross-platform location
+-- 		ts.setup({
+-- 			install_dir = vim.fn.stdpath("data") .. "/site",
+-- 		})
+--
+-- 		-- Install parsers (async). :TSUpdate (above) keeps them in sync.
+-- 		ts.install({
+-- 			-- Front-end
+-- 			"vue", "typescript", "javascript", "html", "css", "scss", "json", "tsx",
+-- 			-- Back-end
+-- 			"c", "cpp", "lua", "python", "rust", "cmake",
+-- 		})
+--
+-- 		-- Enable highlighting via Neovim (main branch expects this)
+-- 		vim.api.nvim_create_autocmd("FileType", {
+-- 			callback = function(ev)
+-- 				-- highlight
+-- 				pcall(vim.treesitter.start, ev.buf)
+--
+-- 				-- indent (experimental), but disable for c/cpp like you had before
+-- 				local ft = vim.bo[ev.buf].filetype
+-- 				if ft ~= "c" and ft ~= "cpp" then
+-- 					vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+-- 				end
+-- 			end,
+-- 		})
+-- 	end,
+-- }
--- lua/plugins/ts-context.lua
+++ lua/plugins/ts-context.lua
@@ -1,1 +1,6 @@
-return { "nvim-treesitter/nvim-treesitter-context", opts = {} }
+return {
+	"nvim-treesitter/nvim-treesitter-context",
+	branch = "master", -- optional, but keeps things explicit
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	opts = {},       -- expands to require("treesitter-context").setup({})
+}
